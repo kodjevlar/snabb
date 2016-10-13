ae95423f23d724c17f6165f6589b5c1866b964ed
Joel Roxell <joel.roxell@na-kd.com>
2016-10-13 16:22:28 +0200

    Update removed git pre-push hook.

--- src/server/generate-markup.js
+++ src/server/generate-markup.js
@@ -20,7 +20,7 @@ export default renderProps => (
 
       <meta
         name='viewport'
-        content='width=device-width, initial-scale=1'
+        content='width=device-width, minimum-scale=1.0'
       />
 
       { process.env.NODE_ENV === 'production' && (
